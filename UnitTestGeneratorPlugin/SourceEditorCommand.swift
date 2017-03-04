5b6560eac97c513bc206c68e8ea69d65f0747328
Taisuke Abe <jormungand2006@gmail.com>
2017-03-04 17:47:34 +0900

    UNIXコマンドでパース結果をクリップボードにコピーする処理の実装と呼び出し

--- UnitTestGeneratorPlugin/SourceEditorCommand.swift
+++ UnitTestGeneratorPlugin/SourceEditorCommand.swift
@@ -19,7 +19,7 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         let textBuffer = invocation.buffer
         // ソースコードの各行を格納してあるNSMutableArray。各要素は Any型。
         let lines = textBuffer.lines
-        print (lines)
+        print(lines)
         
         // TODO: エラー時はErrorを渡す。
 //        let selections = textBuffer.selections
@@ -29,9 +29,32 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
 //        }
         
         // TOOD: 各行をParseして、XCTestCaseファイル（既に存在している前提）にテストメソッドを新規作成できるようにする
+        let parser = SwiftCodeParser()
+        let result = parser.parse(source: textBuffer.completeBuffer)
         
+        let generator = CodeGenerator()
+        let parsedResult = generator.generateTestClass(fromClass: result[0])
+        self.runCopyCommand(result: parsedResult)
 
         // 処理が完了時or何もしなかった時はHandlerにnilを渡す。
         completionHandler(nil)
-    }    
+    }
+    
+    // UNIXコマンドでパース結果をクリップボードにコピーする
+    func runCopyCommand(result: String) {
+        let pipe = Pipe()
+        var task = Process()
+        task.launchPath = "/bin/echo"
+        task.standardOutput = pipe
+        task.arguments = [result]
+        task.launch()
+        task.waitUntilExit()
+        
+        task = Process()
+        task.launchPath = "/usr/bin/pbcopy"
+        task.arguments = ["-c", String(format:"%@", self)]
+        task.standardInput = pipe
+        task.launch()
+        task.waitUntilExit()
+    }
 }
